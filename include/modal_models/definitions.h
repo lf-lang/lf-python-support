b1f1d5ac7b7a10e59c5d237d58f43e21169ddcf6
Steven <stevenhouseng@gmail.com>
2022-04-30 11:48:35 -0700

    update references to renamed file

--- include/modal_models/definitions.h
+++ include/modal_models/definitions.h
@@ -37,7 +37,7 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifdef MODAL_REACTORS
 #include <Python.h>
 #include <structmember.h>
-#include "../ctarget_schedule.h"
+#include "../ctarget/schedule.h"
 
 /**
  * The struct used to represent modes in Python.
